5e1a4eca2077bd0d50895c4395dec11d95807bd5
Bohdan Khablenko <bkhablenko@gmail.com>
2023-04-14 14:38:50 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.gradle.api.tasks.wrapper.Wrapper.DistributionType
+
+plugins {
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.spring") version "1.8.20"
+}
+
+group = "com.github.bkhablenko"
+version = "0.1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+    test {
+        useJUnitPlatform()
+    }
+    wrapper {
+        distributionType = DistributionType.ALL
+        version = "8.1"
+    }
+}
