57c20bf5006ba7dc1e0b4a1e9f53d9c957e00b18
Bohdan Khablenko <bkhablenko@gmail.com>
2023-04-17 00:43:51 +0300

    Remove obsolete methods from ArticleRepository

--- src/main/kotlin/com/github/bkhablenko/upday/domain/repository/ArticleRepository.kt
+++ src/main/kotlin/com/github/bkhablenko/upday/domain/repository/ArticleRepository.kt
@@ -5,11 +5,8 @@ import com.querydsl.core.types.Predicate
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.time.LocalDateTime
 import java.util.UUID
 
 // TODO(bkhablenko): Refactor as a custom repository
@@ -19,11 +16,4 @@ interface ArticleRepository : JpaRepository<ArticleEntity, UUID>, QuerydslPredic
     // Temporary solution to https://stackoverflow.com/questions/43066169
     @EntityGraph(attributePaths = ["authors"])
     override fun findAll(predicate: Predicate, sort: Sort): Iterable<ArticleEntity>
-
-    fun findAllByAuthorsId(authorId: UUID): List<ArticleEntity>
-
-    fun findAllByCreatedDateBetween(fromDate: LocalDateTime, toDate: LocalDateTime): List<ArticleEntity>
-
-    @Query(value = """SELECT * FROM "article" WHERE :tag = ANY("tags")""", nativeQuery = true)
-    fun findAllByTagsContaining(@Param("tag") tag: String): List<ArticleEntity>
 }
